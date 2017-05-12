3413b294345758b851b5d17aac9d340d2b07cd53
Michael <java.dev.m.ar@gmail.com>
2017-05-12 12:39:15 +0300

    add jersey link

--- src/main/java/com/paxxi/routing/machine/prm/MyResource.java
+++ src/main/java/com/paxxi/routing/machine/prm/MyResource.java
@@ -1,14 +1,19 @@
 package com.paxxi.routing.machine.prm;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+
+import org.glassfish.jersey.server.mvc.Viewable;
 
 /**
  * Root resource (exposed at "myresource" path)
  */
-@Path("api")
+//@Path("api")
+@Path("/")
 public class MyResource {
 
     /**
@@ -20,6 +25,15 @@ public class MyResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String getIt() {
-        return "Hello, Heroku2!";
+        return "Hello, Heroku2! https://jersey.java.net/documentation/latest/getting-started.html#new-webapp";
+    }
+    
+    @GET
+    @Path("get")
+    @Produces({MediaType.TEXT_HTML})
+    public Viewable form(
+//    		@Context HttpServletRequest request
+    		) {
+        return new Viewable("/form.jsp", null);
     }
 }
